e4ff392ee6a9e7bea1dae13cad91a3a383c6d61f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-29 10:12:27 -0400

    update the tests a little

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -14,21 +14,5 @@ declare namespace Cypress {
      * cy.createTodo('new item')
      */
     createTodo(title: string): Chainable<any>
-
-    /**
-     * Command that injects Axe core library into app html.
-     * @example
-     *  cy.visit('/')
-     *  cy.v()
-     */
-    addAxeCode(): Chainable<any>
-
-    /**
-     * Run a11y tests or only a subset of all tests
-     * @see https://github.com/avanslaars/cypress-axe
-     * @example
-     *  cy.checkA11y()
-     */
-    checkA11y(any, object): Chainable<any>
   }
 }
