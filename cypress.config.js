e3dcd39dfda10c5b098f7a2d307c5be6a69cdff4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-07-05 12:12:17 -0400

    remove the old plugins file

--- cypress.config.js
+++ cypress.config.js
@@ -12,7 +12,13 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      // optional: register cypress-grep plugin code
+      // https://github.com/bahmutov/cypress-grep
+      require('cypress-grep/src/plugin')(config)
+
+      // make sure to return the config object
+      // as it might have been modified by the plugin
+      return config
     },
     baseUrl: 'http://localhost:8888',
     specPattern: 'cypress/e2e/**/*spec.js',
