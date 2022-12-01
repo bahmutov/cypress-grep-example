63230c7abe6d558bc20788962d49d9880da15acd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-01 16:04:29 -0500

    set omit flag

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,9 @@ module.exports = defineConfig({
   fixturesFolder: false,
   video: false,
   e2e: {
+    env: {
+      grepOmitFiltered: true,
+    },
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
