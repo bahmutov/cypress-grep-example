533cb668d11c524e3ac0153f8dc0fbf217b6da23
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-11 15:01:40 -0400

    k9 solution

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   fixturesFolder: false,
-  projectId: '4agux9',
   video: false,
   env: {
     grepFilterSpecs: true,
@@ -16,10 +15,23 @@ module.exports = defineConfig({
       // https://github.com/bahmutov/cy-grep
       require('@bahmutov/cy-grep/src/plugin')(config)
 
+      console.log('grep tags if any', config.env)
+      // if the user passed grepTags using CYPRESS_grepTags=...
+      // or --env grepTags=... then we can use it
+      // to set the baseUrl to something else
+      // In this example, if the user passed @smoke tag
+      // then set the baseUrl to "https://staging.todomvc.com"
+      if (config.env.grepTags?.includes('@smoke')) {
+        console.log('running smoke tests against staging')
+        config.baseUrl = 'https://staging.todomvc.com'
+      }
+
+      // IMPORTANT:
       // make sure to return the config object
       // as it might have been modified by the plugin
       return config
     },
+    // default base url
     baseUrl: 'http://localhost:8888',
     specPattern: 'cypress/e2e/**/*spec.js',
   },
