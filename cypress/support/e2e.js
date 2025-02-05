aea2bbc49e4c3be10a0e82b8a0f353218c1ad9c8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-02-05 18:05:52 -0500

    added plugin grep boxes

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -5,3 +5,11 @@ require('./commands')
 // load and register the grep feature
 // https://github.com/bahmutov/cy-grep
 require('@bahmutov/cy-grep')()
+
+// https://github.com/dennisbergevin/cypress-plugin-grep-boxes
+const {
+  greppedTestToggle,
+  addGrepButtons,
+} = require('cypress-plugin-grep-boxes')
+greppedTestToggle()
+addGrepButtons()
